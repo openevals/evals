92cd118bcc8df1fa25ab49d3a7e1ce93345ee8fc
Justin W. Lin <justin@lin.ai>
2024-08-04 15:13:58 -0700

    fix: Add model to to `EvalRun`

--- backend/db/models.py
+++ backend/db/models.py
@@ -1,17 +1,28 @@
 from enum import Enum
 
-from sqlalchemy import ARRAY, Boolean, Column
+from sqlalchemy import Boolean, Column
 from sqlalchemy import Enum as SQLAlchemyEnum
-from sqlalchemy import Float, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Float, ForeignKey, Integer, String, Table
+from sqlalchemy.orm import declarative_base, relationship
 
-from backend.db.base import Base
+Base = declarative_base()
+
+# Association table
+eval_authors = Table(
+    "eval_authors",
+    Base.metadata,
+    Column("user_id", Integer, ForeignKey("users.id"), primary_key=True),
+    Column("eval_id", Integer, ForeignKey("evals.id"), primary_key=True),
+)
 
 
 class ValidatorType(Enum):
+    Match = "Match"
+    MultipleChoice = "MultipleChoice"
     ExactMatch = "ExactMatch"
     FuzzyMatch = "FuzzyMatch"
     ModelGraded = "ModelGraded"
+    Custom = "Custom"
 
 
 class User(Base):
@@ -21,7 +32,9 @@ class User(Base):
     email = Column(String, unique=True, nullable=False)
     affiliation = Column(String)
 
-    authored_evals = relationship("Eval", back_populates="authors")
+    authored_evals = relationship(
+        "Eval", secondary=eval_authors, back_populates="authors"
+    )
 
 
 class Model(Base):
@@ -29,6 +42,7 @@ class Model(Base):
     id = Column(Integer, primary_key=True)
     model_developer = Column(String, nullable=False)
     model_name = Column(String, nullable=False)
+    eval_runs = relationship("EvalRun", back_populates="model")
 
 
 class Eval(Base):
@@ -39,15 +53,17 @@ class Eval(Base):
     system_prompt = Column(String)
     validator_type = Column(SQLAlchemyEnum(ValidatorType), nullable=False)
 
-    authors = relationship("User", back_populates="authored_evals")
+    authors = relationship(
+        "User", secondary=eval_authors, back_populates="authored_evals"
+    )
     task_instances = relationship("TaskInstance", back_populates="eval")
     eval_runs = relationship("EvalRun", back_populates="eval")
 
 
 class TaskInstance(Base):
     __tablename__ = "task_instances"
-    is_public = Column(Boolean, nullable=False, default=False)
     id = Column(Integer, primary_key=True)
+    is_public = Column(Boolean, nullable=False, default=False)
     input = Column(String, nullable=False)
     system_prompt = Column(String)  # overrides eval system prompt
     user_prompt = Column(String)  # task-specific user prompt
@@ -64,6 +80,7 @@ class EvalRun(Base):
     id = Column(Integer, primary_key=True)
     score = Column(Float, nullable=False)
     validator_type = Column(SQLAlchemyEnum(ValidatorType), nullable=False)
-
+    model_id = Column(Integer, ForeignKey("models.id"), nullable=False)
+    model = relationship("Model", back_populates="eval_runs")
     eval_id = Column(Integer, ForeignKey("evals.id"), nullable=False)
     eval = relationship("Eval", back_populates="eval_runs")
