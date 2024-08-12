7a97ea185bf00caf45c9a6f54d126f4f974c92ba
Reinier Millo <reinier.millo88@gmail.com>
2024-08-12 14:26:00 -0500

    Fix hook for polling eval runs

--- client/app/lib/hooks/useEvalResults.ts
+++ client/app/lib/hooks/useEvalResults.ts
@@ -5,57 +5,74 @@ import { getEvalRun } from '../../utils/getEvalRun';
 const FINISHED_STATUS = ['Failed', 'Finished'];
 
 const useEvalResults = (evalId: number, evalRunIds: number[]) => {
-  const [evalRuns, setEvalRuns] = useState<any[]>(evalRunIds.map((evalRunId: number) => {
-    return {
-      id: evalRunId,
-      model: {
-        id: 0,
-        modelDeveloper: "",
-        modelName: ""
-      },
-      systemPrompt: "",
-      userPrompt: "",
-      score: 0,
-      datetime: "",
-      validatorType: "",
-      status: "",
-      evalId: 0,
-      taskInstanceOutputs: []
-    }
-  }))
+  const [evalRuns, setEvalRuns] = useState<any[]>([])
   const evalRunsRef = useRef<any[]>()
+
+  console.log("HOOK", evalId, evalRunIds)
 
   /* Keeps reference updated */
   useEffect(() => {
     evalRunsRef.current = evalRuns
+    console.log("Setting state")
   }, [evalRuns])
 
   /* Keep the object state updated */
   const updateObjState = (obj: any) => {
+    if (!evalRunsRef.current) return;
     /* Update object state */
     const newObjs: any[] = evalRunsRef.current!.map((value) => {
       return (value.id === obj.id) ? obj : value;
     });
-    setEvalRuns(newObjs);
+    setEvalRuns([...newObjs]);
   }
 
-  /* Poll the response of the eval run until the object get a finisehd status */
-  const loadUntilFinished = async (evalRunId: number, latency: number) => {
-    let itr = 0;
-    let obj = await getEvalRun(evalId, evalRunId, 0)
-    let lastStatus = obj.status;
-    updateObjState(obj);
-    while (!FINISHED_STATUS.includes(lastStatus)) {
-      obj = await getEvalRun(evalId, evalRunId, latency + 1000 * itr);
-      lastStatus = obj.status;
-      itr += 1;
+
+  useEffect(() => {
+    if (evalRunIds.length === 0) return;
+
+    /* Poll the response of the eval run until the object get a finisehd status */
+    const loadUntilFinished = async (evalRunId: number, latency: number) => {
+      let itr = 0;
+      let obj = await getEvalRun(evalId, evalRunId, 0)
+      let lastStatus = obj.status;
+      console.log(lastStatus);
       updateObjState(obj);
-    }
-  }
+      while (!FINISHED_STATUS.includes(lastStatus)) {
+        obj = await getEvalRun(evalId, evalRunId, latency + 1000 * itr);
+        lastStatus = obj.status;
+        itr += 1;
+        updateObjState(obj);
 
-  evalRunIds.forEach((id: number, idx: number) => {
-    loadUntilFinished(id, 1000 * (idx + 1))
-  });
+      }
+    }
+
+    const newObjs = evalRunIds.map((evalRunId: number) => {
+      return {
+        id: evalRunId,
+        model: {
+          id: 0,
+          modelDeveloper: "",
+          modelName: ""
+        },
+        systemPrompt: "",
+        userPrompt: "",
+        score: 0,
+        datetime: "",
+        validatorType: "",
+        status: "",
+        evalId: 0,
+        taskInstanceOutputs: []
+      }
+    });
+    setEvalRuns(newObjs);
+
+    evalRunIds.forEach((id: number, idx: number) => {
+      loadUntilFinished(id, 1000 * (idx + 1))
+    });
+
+    return () => { }
+  }, [evalId, evalRunIds])
+
 
   return { evalRuns };
 }
