78b12d6d7e0516ee35be43b69ebd3f55c7c635c2
Reinier Millo <reinier.millo88@gmail.com>
2024-08-12 03:13:23 -0500

    Update fields validation

--- backend/validation_schemas/evals.py
+++ backend/validation_schemas/evals.py
@@ -3,11 +3,10 @@ from typing import Optional, List
 from backend.db.models import ValidatorType
 
 
-class ModelSchema(BaseModel):
+class ModelSystemSchema(BaseModel):
     model_id: int = Field(..., alias='modelId')
     system_prompt: Optional[str] = Field(..., alias='systemPrompt')
     user_prompt: Optional[str] = Field(..., alias='userPrompt')
-    validator_type: ValidatorType = Field(..., alias='validatorType')
 
 
 class TaskInstanceSchema(BaseModel):
@@ -22,9 +21,7 @@ class EvalSchema(BaseModel):
     validator_type: ValidatorType = Field(..., alias='validatorType')
     task_instances: List[TaskInstanceSchema] = Field(
         ..., alias='taskInstances')
-    system_prompt: Optional[str] = Field(..., alias='systemPrompt')
-    user_prompt: Optional[str] = Field(..., alias='userPrompt')
-    models: List[ModelSchema]
+    model_system: List[ModelSystemSchema] = Field(..., alias='modelSystem')
 
 
 class EvalResponseSchema(BaseModel):
