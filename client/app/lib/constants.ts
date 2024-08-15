4faf547ea709d4e82157b0a76bd45ca26c8bf3b3
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2024-08-14 19:42:47 -0500

    Add predefined data types
    - Remove non required logs

--- client/app/lib/constants.ts
+++ client/app/lib/constants.ts
@@ -2,6 +2,7 @@ export const MIN_EXAMPLES = 10;
 export const MIN_INSTANCES = 5;
 
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+<<<<<<< HEAD
 
 export const GITHUB_AUTHORIZE_URL = "https://github.com/login/oauth/authorize";
 
@@ -9,3 +10,5 @@ export const GITHUB_SCOPES = [
     "read:user",
     "user:email"
 ];
+=======
+>>>>>>> 5bbafbd (Add predefined data types)
