82bca393d278b95779c34545295ad6f7305feee2
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2024-08-15 16:59:48 -0500

    Include author details to types

--- client/app/lib/types.ts
+++ client/app/lib/types.ts
@@ -61,6 +61,13 @@ export interface IModelSystemResponse {
   userPrompt: string;
 }
 
+export interface IAuthorResponse {
+  username: string;
+  githubId: number;
+  githubLogin: string;
+  githubAvatar: string;
+}
+
 export interface IEvalResponse {
   id: number;
   name: string;
@@ -68,6 +75,7 @@ export interface IEvalResponse {
   validationToken: string;
   taskInstances: ITaskInstanceResponse[];
   modelSystems: IModelSystemResponse[];
+  authors: IAuthorResponse[];
 }
 
 export interface ITaskInstanceOutputResponse {
