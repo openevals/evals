b1066179ccc097cdeca18b0034410bee53f0336c
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2024-08-15 13:21:44 -0500

    Fix invalid merge

--- client/app/lib/constants.ts
+++ client/app/lib/constants.ts
@@ -2,7 +2,6 @@ export const MIN_EXAMPLES = 10;
 export const MIN_INSTANCES = 5;
 
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
-<<<<<<< HEAD
 
 export const GITHUB_AUTHORIZE_URL = "https://github.com/login/oauth/authorize";
 
@@ -10,5 +9,3 @@ export const GITHUB_SCOPES = [
     "read:user",
     "user:email"
 ];
-=======
->>>>>>> 5bbafbd (Add predefined data types)
