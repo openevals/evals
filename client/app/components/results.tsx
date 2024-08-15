098513ce1a523ca5996bb933f860dea679169330
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2024-08-15 15:44:43 -0500

    Upvote an eval

--- client/app/components/results.tsx
+++ client/app/components/results.tsx
@@ -1,30 +1,65 @@
 import ResultItem from "./resultItem";
 import { getEvals } from "../utils/getEvals";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
+import { IRootState } from "@/app/lib/store";
+import { upVoteEval } from "../utils/upVote";
+import { useToast } from "@chakra-ui/react";
+import { IEvalListItemResponse } from "../lib/types";
 
 export default function Results() {
-  const [evals, setEvals] = useState([]);
+  const singleCallRef = useRef(false)
+  const [evals, setEvals] = useState<IEvalListItemResponse[]>([]);
+  const accessToken = useSelector<IRootState, string>((state: IRootState) => state.auth.token);
+  const toast = useToast()
 
   useEffect(() => {
+    if (singleCallRef.current) return;
+    singleCallRef.current = true;
+
     const retrieveEvals = async () => {
-      const ev = await getEvals();
+      const ev = await getEvals(accessToken);
       setEvals(ev);
     };
     retrieveEvals()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const callUpVoteEval = async (evalId: number) => {
+    try {
+      const response = await upVoteEval(accessToken, evalId)
+      setEvals(evals.map((value: any) => {
+        if (value.id !== evalId) return value;
+        value.upvotes = response.upvotes;
+        value.upvoted = response.upvoted;
+        return value
+      }))
+    } catch {
+      toast({
+        title: "Vote failed",
+        description: "There is an error saving your vote. Please, try it again.",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  }
 
   return (
     <>
       {evals.map(({
-        name, description, validatorType
+        id, name, description, validatorType, upvotes, upvoted
       }) => (
         <ResultItem
           key={name}
           name={name}
-          description={description}
+          description={description ?? ''}
           validatorType={validatorType}
+          upvotes={upvotes}
+          upvoted={upvoted}
+          onUpvote={() => callUpVoteEval(id)}
         />
-      ))} 
+      ))}
     </>
   )
 }
---
+++ client/app/utils/upVote.ts
@@ -0,0 +1,19 @@
+
+import { API_URL } from '../lib/constants';
+import { IEvalUpvoteResponse } from '../lib/types';
+
+export async function upVoteEval(accessToken: string, evalId: number): Promise<IEvalUpvoteResponse> {
+  try {
+    const res = await fetch(`${API_URL}/evals/${evalId}/upvote`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'Content-type': 'application/json',
+      }
+    });
+    return await res.json() as IEvalUpvoteResponse
+  } catch (e) {
+    console.error(e);
+    throw e
+  }
+}
