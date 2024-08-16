b66cb1b31cee7c47c810083bdc1d306dbb8a6e92
justanexperiment <justanexperimentpark@gmail.com>
2024-08-16 09:58:28 -0700

    Create MyEvals page

---
+++ client/app/components/myEvals.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { IRootState } from "@/app/lib/store";
+import { getUserEvals, getUserUpvotedEvals } from "../utils/getEvals";
+import { Heading } from "@chakra-ui/react";
+import ResultItem from "./resultItem";
+import { IEvalListItemResponse } from "../lib/types";
+
+export default function MyEvals() {
+  const [userEvals, setUserEvals] = useState<IEvalListItemResponse[]>([]);
+  const [userVotedEvals, setUserVotedEvals] = useState<IEvalListItemResponse[]>([]);
+  const accessToken = useSelector<IRootState, string>((state: IRootState) => state.auth.token);
+
+  useEffect(() => {
+    if (accessToken) {
+      const getUserEvalInfo = async () => {
+        const uEvals: IEvalListItemResponse[]  = await getUserEvals(accessToken);
+        const uUpvotedEvals: IEvalListItemResponse[] = await getUserUpvotedEvals(accessToken);
+        if (uEvals.length > 0) {
+          setUserEvals(userEvals);
+        }
+        if (uUpvotedEvals.length > 0) {
+          setUserVotedEvals(uUpvotedEvals);
+        }
+      };
+      getUserEvalInfo();
+    }
+  }, []);
+
+  return (
+    <>
+      <Heading size='lg'>Your Evals</Heading>
+      <Heading size='md'>Created Evals</Heading>
+      <>
+        {userEvals && userEvals.map(({
+          id, name, description, validatorType, upvotes, upvoted
+        }) => (
+          <ResultItem
+            key={name}
+            name={name}
+            description={description ?? ''}
+            validatorType={validatorType}
+            upvotes={upvotes}
+            upvoted={upvoted}
+            onUpvote={() => { console.log('TODO') }}
+          />
+        ))}
+      </>
+      <Heading size='md'>Upvoted Evals</Heading>
+      <>
+        {userVotedEvals && userVotedEvals.map(({
+          id, name, description, validatorType, upvotes, upvoted
+        }) => (
+          <ResultItem
+            key={name}
+            name={name}
+            description={description ?? ''}
+            validatorType={validatorType}
+            upvotes={upvotes}
+            upvoted={upvoted}
+            onUpvote={() => callUpvoteEval(id)}
+          />
+        ))}
+      </>          
+    </>
+  )
+}
--- client/app/utils/getEvals.ts
+++ client/app/utils/getEvals.ts
@@ -21,3 +21,35 @@ export async function getEvals(accessToken?: string): Promise<IEvalListItemRespo
     throw e;
   }
 }
+
+export async function getUserEvals(accessToken: string): Promise<IEvalListItemResponse[]> {
+  try {
+    const res = await fetch(`${API_URL}/evals/created`, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`
+      },
+    });
+    return await res.json() as IEvalListItemResponse[];
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+}
+
+export async function getUserUpvotedEvals(accessToken: string): Promise<IEvalListItemResponse[]> {
+  try {
+    const res = await fetch(`${API_URL}/evals/upvoted`, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`
+      },
+    });
+    return await res.json() as IEvalListItemResponse[];
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+}
