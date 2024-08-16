7b020908b1196b1a8a76f4a5ba30242755b560d3
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2024-08-16 12:04:24 -0500

    Remove non required tab elements from edit form

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,41 +1,11 @@
-"use client";
-
-import {
-  Tabs, TabList, TabPanels, Tab, TabPanel,
-} from '@chakra-ui/react';
+import type { Metadata } from "next";
 import Editor from "./components/editor";
-import Results from "./components/results";
 
-
-import { useSelector } from 'react-redux';
-import { IRootState } from '@/app/lib/store';
-import { IEvalListItemResponse } from './lib/types';
-import ItemDetails from './components/itemDetails';
+export const metadata: Metadata = {
+  title: "OpenEvals - Create new evals",
+  description: "Evals for the public",
+};
 
 export default function Home() {
-  const evals = useSelector<IRootState, IEvalListItemResponse[]>((state: IRootState) => state.data.evals);
-  return (
-    <Tabs variant='soft-rounded' w='100%' defaultIndex={1}>
-      <TabList>
-        <Tab>{"I'm feeling lucky 🍀"}</Tab>
-        <Tab>Create your own eval (5 min) ⚒️</Tab>
-        <Tab>Contribute to an existing eval! 💛</Tab>
-        <Tab>Vote on important evals 😌</Tab>
-      </TabList>
-      <TabPanels>
-        <TabPanel>
-          <ItemDetails />
-        </TabPanel>
-        <TabPanel>
-          <Editor />
-        </TabPanel>
-        <TabPanel>
-          <Editor />
-        </TabPanel>
-        <TabPanel>
-          <Results evals={evals ?? []} />
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
-  );
+  return (<Editor />);
 }
