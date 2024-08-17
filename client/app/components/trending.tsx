a5774f0299fafc74fdec028e677ebb02f9f26802
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2024-08-17 01:47:52 -0500

    Fix upvote action on my evals and trending views

--- client/app/components/trending.tsx
+++ client/app/components/trending.tsx
@@ -1,24 +1,56 @@
 import { getTopEvals } from "../utils/getEvals";
 import { useEffect, useState } from "react";
 import { IEvalListItemResponse } from "../lib/types";
-import { Divider } from "@chakra-ui/react";
+import { Divider, useToast } from "@chakra-ui/react";
 import ResultItem from "./resultItem";
+import { useDispatch, useSelector } from "react-redux";
+import { IRootState } from "../lib/store";
+import { upvoteEval } from "../utils/upvote";
+import { setUpvotedEval } from "../lib/store/dataSlice";
 
 export default function Trending() {
   const [evals, setEvals] = useState<IEvalListItemResponse[]>([]);
+  const accessToken = useSelector<IRootState, string>((state: IRootState) => state.auth.token);
+  const toast = useToast();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const getTrending = async () => {
-      const evals = await getTopEvals(); 
+      const evals = await getTopEvals();
       setEvals(evals);
     };
     getTrending();
   }, []);
 
+  const callUpvoteEval = async (evalId: number) => {
+    try {
+      const response = await upvoteEval(accessToken, evalId);
+      const payload = { id: evalId, upvotes: response.upvotes, upvoted: response.upvoted };
+      dispatch(setUpvotedEval(payload));
+      setEvals(prevValues => {
+        return prevValues.map((value) => {
+          if (value.id === evalId) {
+            value.upvotes = response.upvotes;
+            value.upvoted = response.upvoted;
+          }
+          return value;
+        });
+      });
+    } catch {
+      toast({
+        title: "Vote failed",
+        description: "There is an error saving your vote. Please, try it again.",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  };
+
   return (
     <>
       {evals.map(({
-        id, name, description, validatorType, upvotes, upvoted
+        id, name, description, validatorType, upvotes, upvoted, authors
       }) => (
         <>
           <ResultItem
@@ -29,7 +61,8 @@ export default function Trending() {
             validatorType={validatorType}
             upvotes={upvotes}
             upvoted={upvoted}
-            onUpvote={() => {}}
+            onUpvote={() => callUpvoteEval(id)}
+            mainAuthor={authors[0]}
           />
           <Divider />
         </>
