19b50e2c9e95e85509cb2f26aeb3464bfc940322
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2024-09-28 22:16:39 -0500

    fe: Add model keys storage provider

--- client/app/providers.tsx
+++ client/app/providers.tsx
@@ -4,13 +4,16 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { store, persistedStore } from '@/app/lib/store';
 import { PersistGate } from 'reduxjs-toolkit-persist/integration/react';
 import { Provider } from 'react-redux';
+import { ModelStorageProvider } from './lib/providers/model-storage';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return <ChakraProvider>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistedStore}>
-        {children}
-      </PersistGate>
+      <ModelStorageProvider>
+        <PersistGate loading={null} persistor={persistedStore}>
+          {children}
+        </PersistGate>
+      </ModelStorageProvider>
     </Provider>
   </ChakraProvider>;
 }
