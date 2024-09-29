392bac4d6394ded0e3a691965a5bd8da66b6c476
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2024-09-28 22:17:39 -0500

    fe: Add key validation call to backend

---
+++ client/app/utils/validateKeys.ts
@@ -0,0 +1,24 @@
+import { API_URL } from "@/app/lib/constants";
+import { IKeyValidationResponse } from "@/app/lib/types";
+
+/**
+ * Validate AI model key
+ * @param provider Model provider: openai, anthropic, google.
+ * @param key API key
+ * @returns 
+ */
+export async function isValidAIModelKey(provider: string, key: string): Promise<boolean> {
+  try {
+    const res = await fetch(`${API_URL}/validate/${provider}`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ key }),
+    });
+    return (await res.json() as IKeyValidationResponse).isValid;
+  } catch (error) {
+    console.error("Error validating AI model key:", error);
+    return false;
+  }
+};
